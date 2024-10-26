1ac9435425606889f13e2865592b549ee314699b
fikretellek <fikretellek@icloud.com>
2024-10-26 18:06:46 +0100

    api implemented for sign up route

--- api/api.js
+++ api/api.js
@@ -1,13 +1,36 @@
 import { Router } from "express";
 
 import messageRouter from "./messages/messageRouter.js";
+import clientRoutes from "./routes/clientRoutes.js";
 
 const api = Router();
 
 api.use("/message", messageRouter);
 
-api.get("/test", (req, res) => {
-	res.send("hiiiiiiiiiiii");
+api.use("/sign-up", clientRoutes);
+
+api.get("/settings", (req, res) => {
+	res.send("not implemented yet");
+});
+
+api.get("/user", (req, res) => {
+	res.send("not implemented yet");
+});
+
+api.get("/user/set", (req, res) => {
+	res.send("not implemented yet");
+});
+
+api.get("/user/get", (req, res) => {
+	res.send("not implemented yet");
+});
+
+api.get("/user/update", (req, res) => {
+	res.send("not implemented yet");
+});
+
+api.get("/user/delete", (req, res) => {
+	res.send("not implemented yet");
 });
 
 export default api;
