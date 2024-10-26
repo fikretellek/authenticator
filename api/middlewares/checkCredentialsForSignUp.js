b69e66eb2978be7417d47ff0b427a9039a585811
fikretellek <fikretellek@icloud.com>
2024-10-26 22:44:57 +0100

    middleware for checking credentials for signing up implemented

---
+++ api/middlewares/checkCredentialsForSignUp.js
@@ -0,0 +1,24 @@
+export const checkCredentialsForSignUp = (req, res, next) => {
+	const { email, password } = req.body;
+
+	const emailRegex = /^[\w.-]+@([\w-]+\.)+[\w-]{2,4}$/;
+	if (!emailRegex.test(email)) {
+		return res.status(400).json({ error: "Invalid email format." });
+	}
+
+	const isPasswordValid =
+		password.length >= 8 &&
+		/[A-Z]/.test(password) &&
+		/[a-z]/.test(password) &&
+		/\d/.test(password) &&
+		/[!@#$%^&*(),.?":{}|<>]/.test(password);
+
+	if (!isPasswordValid) {
+		return res.status(400).json({
+			error:
+				"Password must be at least 8 characters long, contain uppercase and lowercase letters, a number, and a special character.",
+		});
+	}
+
+	next();
+};
--- api/routes/clientRoutes.js
+++ api/routes/clientRoutes.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
 
 import clientController from "../controllers/clientController/index.js";
+import { checkCredentialsForSignUp } from "../middlewares/checkCredentialsForSignUp.js";
 
 const router = Router();
 
-router.get("/sign-up", clientController.createNewClient);
+router.get(
+	"/sign-up",
+	checkCredentialsForSignUp,
+	clientController.createNewClient,
+);
 
 export default router;
