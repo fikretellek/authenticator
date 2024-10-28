9ec5be90eca2bb0f7579e89f20fdd88552a52fd6
fikretellek <fikretellek@icloud.com>
2024-10-28 21:56:17 +0000

    try catch block disabled for debugging purposes

--- api/controllers/clientController/createNewClient/createNewClient.js
+++ api/controllers/clientController/createNewClient/createNewClient.js
@@ -4,22 +4,22 @@ import { hashPassword } from "../../../functions/hashPassword.js";
 export const createNewClient = async (req, res) => {
 	const { email, password } = req.body;
 
-	try {
-		const passwordHash = await hashPassword(password);
+	// try {
+	const passwordHash = await hashPassword(password);
 
-		// this is way to compare password for future references "const isMatch = await bcrypt.compare(password, user.password);"
-		const saveClientQuery = await db.query(
-			"INSERT INTO clients (email, password_hash, api_key) VALUES ($1, $2, uuid_generate_v4()) RETURNING api_key",
-			[email, passwordHash],
-		);
+	// this is way to compare password for future references "const isMatch = await bcrypt.compare(password, user.password);"
+	const saveClientQuery = await db.query(
+		"INSERT INTO clients (email, password_hash, api_key) VALUES ($1, $2, uuid_generate_v4()) RETURNING api_key",
+		[email, passwordHash],
+	);
 
-		const apiKey = saveClientQuery.rows[0].api_key;
+	const apiKey = saveClientQuery.rows[0].api_key;
 
-		res.status(201).send({ success: true, apiKey: apiKey });
-	} catch {
-		res.status(500).send({
-			success: false,
-			error: "An error occurred while creating the client.",
-		});
-	}
+	res.status(201).send({ success: true, apiKey: apiKey });
+	// } catch {
+	// 	res.status(500).send({
+	// 		success: false,
+	// 		error: "An error occurred while creating the client.",
+	// 	});
+	// }
 };
